aa80e4d8d19f0c210ebdc7be85142adcad4fa41d
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-05-23 11:54:00 +0600

    show loader when navigating to routes

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
+import Spinner from "../components/ui/Spinner";
 
 const MainLayout = () => {
+  const navigation = useNavigation();
+  const isNavigating = Boolean(navigation.location);
   return (
     <div>
       <Header />
-      <div>
-        <Outlet />
-      </div>
+      {isNavigating ? (
+        <Spinner />
+      ) : (
+        <div>
+          <Outlet />
+        </div>
+      )}
       <Footer />
     </div>
   );
